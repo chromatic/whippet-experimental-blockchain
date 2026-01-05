f63c20e320533a28099f64b3e41578c0374f214d
chromatic <chromatic@wgz.org>
2026-01-05 11:43:58 -0800

    Report seconds in transaction overview
    Because block mining time is ~6 seconds, show seconds as well.

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -86,7 +86,8 @@ namespace GUIUtil {
 
 QString dateTimeStr(const QDateTime &date)
 {
-    return date.date().toString(Qt::SystemLocaleShortDate) + QString(" ") + date.toString("hh:mm");
+    // Show seconds so transaction times are precise in the overview list
+    return date.date().toString(Qt::SystemLocaleShortDate) + QString(" ") + date.toString("hh:mm:ss");
 }
 
 QString dateTimeStr(qint64 nTime)
